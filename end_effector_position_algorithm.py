e83c417ad195d449152e8e1a84f34b572f137f6f
William Roche <74993912+williamjroche@users.noreply.github.com>
2025-10-19 00:06:25 -0400

    Add files via upload

---
+++ end_effector_position_algorithm.py
@@ -0,0 +1,41 @@
+import numpy as np
+from sympy import Symbol, cos as c, sin as s, pi
+
+theta1 = Symbol('theta1')
+theta2 = Symbol('theta2')
+theta3 = Symbol('theta3')
+L1 = Symbol('L1')
+
+phi = 180
+x = input("Enter the X coordinate: ")
+y = input("Enter the Y coordinate: ")
+z = input("Enter the Z coordinate: ")
+l1 = input("Enter L1: ")
+
+def transform_matrix(alpha, a, d, theta):
+    T = np.array([[c(theta), -s(theta), 0, a],
+                 [s(theta)*c(alpha), c(theta)*c(alpha), -s(alpha), -s(alpha)*d], 
+                 [s(theta)*s(alpha), c(theta)*s(alpha), c(alpha), c(alpha)*d],
+                 [0, 0, 0, 1]])
+    return T
+
+#Link parameters / DH table
+dh = np.array([[0, 0, 0, theta1], [-pi/2, 0, 0, theta2], [0, L1, 0, theta3]])
+
+#needed transform matrices
+T0_1 = transform_matrix(dh[0][0], dh[0][1], dh[0][2], dh[0][3])
+
+T1_2 = transform_matrix(dh[1][0], dh[1][1], dh[1][2], dh[1][3])
+
+T2_3 = transform_matrix(dh[2][0], dh[2][1], dh[2][2], dh[2][3])
+
+T0_2 = np.matmul(T0_1, T1_2)
+
+#Transform matrix from base to end effector
+T0_3 = np.matmul(T0_2, T2_3)
+
+#IK equations
+X_pos = T0_3[0][3]
+Y_pos = T0_3[1][3]
+Z_pos = T0_3[2][3]
+
